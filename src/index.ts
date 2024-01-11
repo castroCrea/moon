13f889cee2181e4fccb3c7cbe045c8b56c0b78b4
Castro Crea <castro.crea@gmail.com>
2024-01-11 12:36:09 +0100

    📝 Add eslint and improve typing

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
-import { Context } from './FetchContext.type';
+import { type Context } from './FetchContext.type'
 
 export * from './FetchContext.type'
 
 export type PluginSettingsDescription = Record<string, {
-  type: 'string';
-  required: boolean;
-  label: string;
-  description: string;
-}>;
+  type: 'string'
+  required: boolean
+  label: string
+  description: string
+}>
 
-export type MoonPluginSettings = Record<string, string>;
+export type MoonPluginSettings = Record<string, string>
 
 export class MoonPlugin {
   /**
    * name - The name of the plugin
    */
-  name: string = 'Plugin name';
+  name: string = 'Plugin name'
 
   /**
    * logo - The logo of the plugin
    */
-  logo: string = 'https://www.image.so/images/favicon.ico';
+  logo: string = 'https://www.image.so/images/favicon.ico'
 
   /**
    * Describe the settings for the plugin that will appears in the settings modal
-   * 
+   *
    * example
    * {
    *   token: {
@@ -35,57 +35,61 @@ export class MoonPlugin {
    *   },
    * },
    */
-  settingsDescription: PluginSettingsDescription = {};
-  
+  settingsDescription: PluginSettingsDescription = {}
+
   /**
    * settings - The settings of the plugin
    */
-  settings: MoonPluginSettings = {};
+  settings: MoonPluginSettings = {}
 
   /**
    * constructor - Create a new instance of the plugin
-   * 
+   *
    * @param settings - The settings of the plugin
    */
-  constructor(props?: { settings: MoonPluginSettings }) {
-    if (!props) return;
-    this.settings = props.settings;
+  constructor (props?: { settings: MoonPluginSettings }) {
+    if (props == null) return
+    this.settings = props.settings
   }
 
   /**
    * saveSettings - Save the settings to the plugin's settings file
-   * 
-   * @param settings 
+   *
+   * @param settings
    * @param writeSettings
    *        @param settings
    *        @param settingsFileName - The name with no extension, by default will take the name of the plugin from settings
    */
-  async saveSettings({ settings, writeSettings }: { settings: MoonPluginSettings, writeSettings: <MoonPluginSettings>({ settings, settingsFileName }: {
-    settings: MoonPluginSettings;
-    settingsFileName: string;
-  }) => void}) {
-    writeSettings({ settings, settingsFileName: this.name }); 
-    this.settings = settings;
+  async saveSettings ({ settings, writeSettings }: {
+    settings: MoonPluginSettings
+    writeSettings: <MoonPluginSettings>({ settings, settingsFileName }: {
+      settings: MoonPluginSettings
+      settingsFileName: string
+    }) => void
+  }): Promise<void> {
+    writeSettings({ settings, settingsFileName: this.name })
+    this.settings = settings
   }
 
   /**
    * If you want to add a new integration, you can add it here
-   * 
+   *
    * This will be called on output
    */
-  async integration(props: {markdown: string}) {
-    console.log('MoonPlugin integration');
+  async integration (props: { markdown: string }): Promise<boolean> {
+    console.log('MoonPlugin integration')
+    return false
   }
 
   /**
    * If you want to add a new context, you can add it here
-   * 
+   *
    * This will be called on input and added to context
-   * 
+   *
    * @return Context
    */
-  async context(): Promise<Partial<Context>> {
-    console.log('MoonPlugin integration');
-    return {people: [], source:{}}
+  async context (): Promise<Partial<Context>> {
+    console.log('MoonPlugin integration')
+    return { people: [], source: {} }
   }
 }
